8e71f32edd289d26ca2f9f8638684cc19cbc8b6c
betultaskiran <betultaskiran34@gmail.com>
2024-07-21 23:29:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,220 @@
+const mealsEl = document.getElementById("meals");
+const favoriteContainer = document.getElementById("fav-meals");
+const mealPopup = document.getElementById("meal-popup");
+const mealInfoEl = document.getElementById("meal-info");
+const popupCloseBtn = document.getElementById("close-popup");
+
+const searchTerm = document.getElementById("search-term");
+const searchBtn = document.getElementById("search");
+
+getRandomMeal();
+fetchFavMeals();
+
+async function getRandomMeal() {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+  const respData = await resp.json();
+  const randomMeal = respData.meals[0];
+
+  addMeal(randomMeal, true);
+}
+
+async function getMealById(id) {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+  );
+
+  const respData = await resp.json();
+  const meal = respData.meals[0];
+
+  return meal;
+}
+
+async function getMealsBySearch(term) {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
+  );
+
+  const respData = await resp.json();
+  const meals = respData.meals;
+
+  return meals;
+}
+
+function addMeal(mealData, random = false) {
+  console.log(mealData);
+
+  const meal = document.createElement("div");
+  meal.classList.add("meal");
+
+  meal.innerHTML = `
+        <div class="meal-header">
+            ${
+              random
+                ? `
+            <span class="random"> Random Recipe </span>`
+                : ""
+            }
+            <img
+                src="${mealData.strMealThumb}"
+                alt="${mealData.strMeal}"
+            />
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button class="fav-btn">
+                <i class="fas fa-heart"></i>
+            </button>
+        </div>
+    `;
+  ///*mealData nesnesi içindeki strMealThumb özelliğine erişimi sağlar. Bu özellik, yemek tarifinin küçük resim URL'sini içerir.*/
+  const btn = meal.querySelector(".meal-body .fav-btn");
+
+  btn.addEventListener("click", () => {
+    if (btn.classList.contains("active")) {
+      removeMealLS(mealData.idMeal);
+      btn.classList.remove("active");
+    } else {
+      addMealLS(mealData.idMeal);
+      btn.classList.add("active");
+    }
+
+    fetchFavMeals();
+  });
+
+  meal.addEventListener("click", () => {
+    showMealInfo(mealData);
+  });
+
+  mealsEl.appendChild(meal);
+}
+//Yerel Depolamaya Yemek Tarifi Ekleme
+function addMealLS(mealId) {
+  const mealIds = getMealsLS();
+
+  localStorage.setItem("mealIds", JSON.stringify([...mealIds, mealId]));
+}
+//Yerel Depolamadan Yemek Tarifi Kaldırma
+function removeMealLS(mealId) {
+  const mealIds = getMealsLS();
+
+  localStorage.setItem(
+    "mealIds",
+    JSON.stringify(mealIds.filter((id) => id !== mealId))
+  );
+  /*filter metodu, orijinal diziyi filtreleyerek belirli bir koşula uyan öğeleri içeren yeni bir dizi oluşturur.*/
+}
+//Yerel Depolamadan Yemek Tariflerini Alma   //cookie ve localstorage öğren!!!
+function getMealsLS() {
+  const mealIds = JSON.parse(localStorage.getItem("mealIds"));
+
+  return mealIds === null ? [] : mealIds;
+}
+
+async function fetchFavMeals() {
+  // clean the container
+  favoriteContainer.innerHTML = "";
+
+  const mealIds = getMealsLS();
+
+  for (let i = 0; i < mealIds.length; i++) {
+    const mealId = mealIds[i];
+    meal = await getMealById(mealId);
+
+    addMealFav(meal);
+  }
+}
+
+function addMealFav(mealData) {
+  const favMeal = document.createElement("li");
+
+  favMeal.innerHTML = `
+        <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+        /><span>${mealData.strMeal}</span>
+        <button class="clear"><i class="fas fa-window-close"></i></button>
+    `;
+
+  const btn = favMeal.querySelector(".clear");
+
+  btn.addEventListener("click", () => {
+    removeMealLS(mealData.idMeal);
+
+    fetchFavMeals();
+  });
+
+  favMeal.addEventListener("click", () => {
+    showMealInfo(mealData);
+  });
+
+  favoriteContainer.appendChild(favMeal);
+}
+
+function showMealInfo(mealData) {
+  // clean it up
+  mealInfoEl.innerHTML = "";
+
+  // update the Meal info
+  const mealEl = document.createElement("div");
+
+  const ingredients = [];
+
+  // get ingredients and measures
+  for (let i = 1; i <= 20; i++) {
+    if (mealData["strIngredient" + i]) {
+      ingredients.push(
+        `${mealData["strIngredient" + i]} - ${mealData["strMeasure" + i]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  mealEl.innerHTML = `
+        <h1>${mealData.strMeal}</h1>
+        <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+        />
+        <p>
+        ${mealData.strInstructions}
+        </p>
+        <h3>Ingredients:</h3>
+        <ul>
+            ${ingredients
+              .map(
+                (ing) => `
+            <li>${ing}</li>
+            `
+              )
+              .join("")}
+        </ul>
+    `;
+  /*map Metodu: Bir dizideki her bir öğe için belirtilen callback fonksiyonunu çalıştırarak yeni bir dizi oluşturur.
+     join Metodu: Bir dizinin tüm öğelerini birleştirerek tek bir string oluşturur.*/
+
+  mealInfoEl.appendChild(mealEl);
+
+  // show the popup
+  mealPopup.classList.remove("hidden");
+}
+
+searchBtn.addEventListener("click", async () => {
+  // clean container
+  mealsEl.innerHTML = "";
+
+  const search = searchTerm.value;
+  const meals = await getMealsBySearch(search);
+
+  if (meals) {
+    meals.forEach((meal) => {
+      addMeal(meal);
+    });
+  }
+});
+
+popupCloseBtn.addEventListener("click", () => {
+  mealPopup.classList.add("hidden");
+});
